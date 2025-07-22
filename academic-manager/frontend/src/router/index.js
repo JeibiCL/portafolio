a04eee80a709c53c327fd433cb15955e724dc123
JAVIERA <javi.villagras@uc.cl>
2025-07-21 20:45:09 -0400

    feat:init frontend

---
+++ academic-manager/frontend/src/router/index.js
@@ -0,0 +1,16 @@
+// frontend/src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginView from '../views/LoginView.vue';
+import DashboardView from '../views/DashboardView.vue';
+
+const routes = [
+  { path: '/', name: 'Login', component: LoginView },
+  { path: '/dashboard', name: 'Dashboard', component: DashboardView },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
